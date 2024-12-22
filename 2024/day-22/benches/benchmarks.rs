f4a78dc93a9fa5deb9368fc067d83be0d1df6df2
lunar-mycroft <lunar-mycroft@users.noreply.github.com>
2024-12-22 12:21:31 -0500

    added pre-allocated version

--- 2024/day-22/benches/benchmarks.rs
+++ 2024/day-22/benches/benchmarks.rs
@@ -85,3 +85,18 @@ fn part2_fxhash(bencher: divan::Bencher) {
                 .pipe_ref(part2::fxhash_cache)
         });
 }
+
+#[divan::bench]
+fn part2_prealloc(bencher: divan::Bencher) {
+    bencher
+        .with_inputs(|| {
+            common::read_input!("part2.txt")
+                .parse::<Puzzle>()
+                .map(divan::black_box)
+        })
+        .bench_values(|res| {
+            res.expect("parsing to suceed")
+                .pipe(divan::black_box)
+                .pipe_ref(part2::pre_alloc)
+        });
+}
--- 2024/day-22/src/part2.rs
+++ 2024/day-22/src/part2.rs
@@ -8,6 +8,26 @@ use crate::{Puzzle, Rng};
 
 #[must_use]
 pub fn initial(puzzle: &Puzzle) -> u64 {
+    fn buy_prices(seed: u32) -> HashMap<(i8, i8, i8, i8), i8> {
+        Rng(seed).take(2001).map(price).tuple_windows().fold(
+            HashMap::new(),
+            |mut map, (a, b, c, d, e)| {
+                let seq = (b - a, c - b, d - c, e - d);
+                map.entry(seq).or_insert(e);
+                map
+            },
+        )
+    }
+
+    fn sequences(seed: u32) -> impl Iterator<Item = (i8, i8, i8, i8)> {
+        Rng(seed)
+            .take(610)
+            .map(price)
+            .tuple_windows()
+            .map(|(first, second)| (second - first))
+            .tuple_windows()
+    }
+
     let all_sequences = puzzle
         .numbers
         .par_iter()
@@ -92,6 +112,26 @@ pub fn fxhash_cache(puzzle: &Puzzle) -> u64 {
         .unwrap_or(0)
 }
 
+#[must_use]
+pub fn pre_alloc(puzzle: &Puzzle) -> u64 {
+    // there are 40,951 total possible windows, so pre-allocate all of them
+    let mut cache: HashMap<u32, Entry> = HashMap::with_capacity(40_951);
+    for (idx, seed) in puzzle.numbers.iter().copied().enumerate() {
+        for (a, b, c, d, e) in Rng(seed).take(2001).map(price).tuple_windows() {
+            cache
+                .entry([b - a, c - b, c - d, e - d].pipe(int_key))
+                .or_default()
+                .note_at(idx, e);
+        }
+    }
+
+    cache
+        .into_values()
+        .map(|entry| entry.total_price)
+        .max()
+        .unwrap_or(0)
+}
+
 #[inline]
 fn int_key(deltas: [i8; 4]) -> u32 {
     deltas.map(|b| b.to_ne_bytes()[0]).pipe(u32::from_ne_bytes)
@@ -119,26 +159,6 @@ impl Entry {
     }
 }
 
-fn buy_prices(seed: u32) -> HashMap<(i8, i8, i8, i8), i8> {
-    Rng(seed).take(2001).map(price).tuple_windows().fold(
-        HashMap::new(),
-        |mut map, (a, b, c, d, e)| {
-            let seq = (b - a, c - b, d - c, e - d);
-            map.entry(seq).or_insert(e);
-            map
-        },
-    )
-}
-
-fn sequences(seed: u32) -> impl Iterator<Item = (i8, i8, i8, i8)> {
-    Rng(seed)
-        .take(610)
-        .map(price)
-        .tuple_windows()
-        .map(|(first, second)| (second - first))
-        .tuple_windows()
-}
-
 fn price(seed: u32) -> i8 {
     i8::try_from(seed % 10).expect("10 < i32::MAX")
 }
@@ -156,9 +176,10 @@ mod tests {
     fn finds_solution(#[case] input_path: &str, #[case] expected: u64) -> Result<()> {
         let input: Puzzle = common::read_input!(input_path).parse()?;
         // assert_eq!(initial(&input), expected);
-        assert_eq!(one_pass(&input), expected);
-        assert_eq!(u32_key(&input), expected);
-        assert_eq!(fxhash_cache(&input), expected);
+        // assert_eq!(one_pass(&input), expected);
+        // assert_eq!(u32_key(&input), expected);
+        // assert_eq!(fxhash_cache(&input), expected);
+        assert_eq!(pre_alloc(&input), expected);
         Ok(())
     }
 }
