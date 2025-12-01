339982462dcc9deeba064ad3a347d46c5642b648
lunar-mycroft <lunar-mycroft@users.noreply.github.com>
2025-12-01 03:02:28 -0500

    further shuffling

--- 2025/day-01/src/part2.rs
+++ 2025/day-01/src/part2.rs
@@ -1,24 +1,23 @@
-use core::panic;
+use tap::prelude::*;
 
 use crate::Puzzle;
 
 #[must_use]
-#[allow(clippy::needless_pass_by_value, clippy::cast_sign_loss)]
+#[allow(clippy::needless_pass_by_value)]
 pub fn process(puzzle: Puzzle) -> u64 {
     let mut position: i16 = 50;
     puzzle
         .rotations
         .into_iter()
         .map(|rotation| {
-            let (full_rotations, extra) = ((rotation / 100).abs(), rotation % 100);
-            let new = (position + extra).rem_euclid(100);
-            let delta = match (position + extra, position) {
+            let (full_rotations, delta) = ((rotation / 100).unsigned_abs(), rotation % 100);
+            let zeros: u64 = match (position + delta, position) {
                 (0, _) => 1,
-                (_, 0) | (0..=99, _) => 0,
+                (_, 0) | (1..=99, _) => 0,
                 (_, _) => 1,
             };
-            position = new;
-            delta + (full_rotations as u64)
+            position = (position + delta).rem_euclid(100);
+            zeros + full_rotations.conv::<u64>()
         })
         .sum()
 }
