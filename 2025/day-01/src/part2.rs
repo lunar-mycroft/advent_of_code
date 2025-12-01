69f087b8772061a0e3e3d0457640134a375d78bb
lunar-mycroft <lunar-mycroft@users.noreply.github.com>
2025-12-01 02:55:05 -0500

    cleaned up part2

--- 2025/day-01/src/part2.rs
+++ 2025/day-01/src/part2.rs
@@ -1,22 +1,26 @@
+use core::panic;
+
 use crate::Puzzle;
 
 #[must_use]
 #[allow(clippy::needless_pass_by_value, clippy::cast_sign_loss)]
 pub fn process(puzzle: Puzzle) -> u64 {
     let mut position: i16 = 50;
-    puzzle.rotations.into_iter().fold(0, |acc, rotation| {
-        let (delta, new) = {
+    puzzle
+        .rotations
+        .into_iter()
+        .map(|rotation| {
             let (full_rotations, extra) = ((rotation / 100).abs(), rotation % 100);
-            match (position + extra, position) {
-                (0, _) => (full_rotations as u64 + 1, 0),
-                (n, 0) => (full_rotations as u64, n.rem_euclid(100)),
-                (n @ 0..=99, _) => (full_rotations as u64, n),
-                (n, _) => ((full_rotations as u64) + 1, n.rem_euclid(100)),
-            }
-        };
-        position = new;
-        acc + delta
-    })
+            let new = (position + extra).rem_euclid(100);
+            let delta = match (position + extra, position) {
+                (0, _) => 1,
+                (_, 0) | (0..=99, _) => 0,
+                (_, _) => 1,
+            };
+            position = new;
+            delta + (full_rotations as u64)
+        })
+        .sum()
 }
 
 #[cfg(test)]
@@ -32,7 +36,6 @@ mod tests {
     fn finds_solution(#[case] input_path: &str, #[case] expected: u64) -> Result<()> {
         let input: Puzzle = common::read_input!(input_path).parse()?;
         let output = process(input);
-        // not 7175 or 6967
         assert_eq!(output, expected);
         Ok(())
     }
