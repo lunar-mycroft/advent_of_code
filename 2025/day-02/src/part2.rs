2a531dd214aa38c8ebd9a29d3f0e9d06fa987ee1
lunar-mycroft <lunar-mycroft@users.noreply.github.com>
2025-12-02 01:53:06 -0500

    d2p2

--- 2025/day-02/src/part2.rs
+++ 2025/day-02/src/part2.rs
@@ -2,6 +2,7 @@ use color_eyre::{
     eyre::{bail, WrapErr},
     Result,
 };
+use itertools::Itertools;
 use tap::prelude::*;
 
 use crate::Puzzle;
@@ -9,24 +10,38 @@ use crate::Puzzle;
 #[must_use]
 #[allow(clippy::needless_pass_by_value)]
 pub fn process(puzzle: Puzzle) -> Result<u64> {
-    todo!("day_02 part2")
+    puzzle
+        .ranges
+        .iter()
+        .filter(|(start, end)| !(start.is_empty() || end.is_empty()))
+        .map(|(start, end)| {
+            repeats(start, end)
+                .wrap_err_with(|| format!("{start:?}, {end:?}"))?
+                .sum::<u64>()
+                .pipe(Ok::<_, color_eyre::Report>)
+        })
+        .try_fold(0, |sum, res| Ok(sum + res?))
+}
+
+fn repeats_n(len: u32, start_n: u64, end_n: u64, repetitions: u32) -> impl Iterator<Item = u64> {
+    let half = 10u64.pow((len / repetitions).max(1) - 1);
+    (half..)
+        .map(move |n| {
+            (0..repetitions)
+                .map(|p| 10u64.pow((n.checked_ilog10().unwrap_or(0) + 1) * p) * n)
+                .sum::<u64>()
+        })
+        .skip_while(move |&n| n < start_n)
+        .take_while(move |&n| n <= end_n)
 }
 
 fn repeats(start: &str, end: &str) -> Result<impl Iterator<Item = u64>> {
-    let end: u64 = end.parse()?;
-    let half = 10u64.pow((start.len().try_conv::<u32>()? / 2).max(1) - 1);
-    let start: u64 = start.parse().wrap_err("start")?;
-    (half..)
-        .flat_map(|n| {
-            let digits = n.checked_ilog10().unwrap_or(0) + 1;
-            (2u32..).map(move |p| {
-                (1..=p)
-                    .map(|q| 10u64.strict_pow(q * digits) * n)
-                    .sum::<u64>()
-            })
-        })
-        .skip_while(move |&n| n < start)
-        .take_while(move |&n| n <= end)
+    let start_n: u64 = start.parse()?;
+    let end_n: u64 = end.parse()?;
+    let len: u32 = start.len().try_conv()?;
+    (2..=end.len().try_conv::<u32>()?.max(len))
+        .flat_map(move |n| repeats_n(len, start_n, end_n, n))
+        .unique()
         .pipe(Ok)
 }
 
@@ -48,15 +63,15 @@ mod tests {
     }
 
     #[rstest]
-    #[case("11", "22", vec![11, 22])]
-    #[case("95", "115", vec![99, 111])]
-    #[case("998", "1012", vec![999, 1010])]
-    #[case("1188511880", "1188511890", vec![1_188_511_885])]
-    #[case("222220", "222224", vec![222_222])]
-    #[case("1698522", "1698528", vec![])]
-    #[case("446443", "446449", vec![446_446])]
-    #[case("38593856", "38593862", vec![38_593_859])]
-    #[case("3", "17", vec![11])]
+    #[case::a("11", "22", vec![11, 22])]
+    #[case::b("95", "115", vec![99, 111])]
+    #[case::c("998", "1012", vec![999, 1010])]
+    #[case::d("1188511880", "1188511890", vec![1_188_511_885])]
+    #[case::e("222220", "222224", vec![222_222])]
+    #[case::f("1698522", "1698528", vec![])]
+    #[case::g("446443", "446449", vec![446_446])]
+    #[case::h("38593856", "38593862", vec![38_593_859])]
+    #[case::i("3", "17", vec![11])]
     fn repeat_works(
         #[case] start: &str,
         #[case] end: &str,
