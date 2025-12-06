64e4c5cc2b9cdeb320ba084da931d998de137f03
lunar-mycroft <lunar-mycroft@users.noreply.github.com>
2025-12-06 03:44:05 -0500

    part 2 parser cleanup

--- 2025/day-06/src/part2.rs
+++ 2025/day-06/src/part2.rs
@@ -1,4 +1,6 @@
-use color_eyre::eyre::{bail, ensure, eyre, Report, Result};
+use color_eyre::eyre::{bail, eyre, OptionExt, Result};
+use common::grid::{self, Grid};
+use glam::IVec2;
 use itertools::Itertools;
 use tap::prelude::*;
 
@@ -19,73 +21,60 @@ pub fn process(puzzle: Puzzle) -> u64 {
 
 impl Puzzle {
     pub fn parse_part_2(s: &str) -> Result<Self> {
-        #[derive(Debug, Clone, Copy)]
-        enum Operator {
-            Add,
-            Mul,
-        }
-        let rows = s.trim().lines().collect_vec();
-        ensure!(rows.len() > 1, "Input too short");
-        ensure!(
-            rows.iter().rev().skip(1).copied().map(str::len).all_equal(),
-            "Non-rectangular input"
+        let (nums, ops) = s
+            .trim()
+            .rsplit_once('\n')
+            .ok_or_eyre("Input only one row")?;
+        let size = IVec2 {
+            x: nums
+                .lines()
+                .map(|s| s.trim_matches('\r'))
+                .map(str::len)
+                .unique()
+                .at_most_one()
+                .map_err(|_| grid::Error::InconsistentLines)?
+                .ok_or(grid::Error::Empty)?
+                .try_conv()?,
+            y: nums.lines().count().try_conv()?,
+        };
+        let grid = Grid::from_row_major_ordered(
+            nums.as_bytes()
+                .iter()
+                .copied()
+                .filter(|&b| !matches!(b, b'\r' | b'\n')),
+            size,
         );
-        let last: Vec<_> = rows
-            .last()
-            .expect("Verified non-empty")
-            .char_indices()
-            .map(|(idx, c)| match c {
-                ' ' => Ok(None),
-                '+' => Some((idx, Operator::Add)).pipe(Ok),
-                '*' => Some((idx, Operator::Mul)).pipe(Ok),
-                c => bail!("{c:?} is not a valid character"),
-            })
-            .filter_map_ok(std::convert::identity)
-            .try_collect()?;
-        let digits = rows[..rows.len() - 1]
-            .iter()
-            .copied()
-            .map(|s| {
-                let mut v = last
-                    .iter()
-                    .tuple_windows::<(_, _)>()
-                    .map(|(&(start, _), &(end, _))| &s[start..end - 1])
-                    .collect_vec();
-                let start = last.last().expect("Known non-empty").0;
-                v.push(&s[start..]);
-                v
-            })
-            .collect_vec();
         Self {
-            problems: last
-                .into_iter()
-                .enumerate()
-                .map(|(x, (_, op))| {
-                    let digits = digits.iter().rev().map(|row| row[x]).collect_vec();
-                    ensure!(digits.iter().copied().map(str::len).all_equal());
-                    let width = digits.first().expect("Known non-empty").len();
-                    let nums: Vec<_> = (0..width)
-                        .map(|x| {
-                            digits
-                                .iter()
-                                .rev()
-                                .map(|b| b.as_bytes()[x])
-                                .filter_map(|b| match b {
-                                    digit @ b'0'..=b'9' => Ok(digit - b'0').pipe(Some),
-                                    b' ' => None,
-                                    other => {
-                                        eyre!("{other} is not a valid digit").pipe(Err).pipe(Some)
-                                    }
-                                })
-                                .map_ok(u64::from)
-                                .try_fold(0, |acc, res| Ok::<_, Report>(acc * 10 + res?))
+            problems: (0..grid.size().x)
+                .map(|x| {
+                    (0..grid.size().y)
+                        .map(|y| grid[IVec2::new(x, y)])
+                        .filter_map(|d| match d {
+                            d @ b'0'..=b'9' => Ok(d - b'0').pipe(Some),
+                            b' ' => None,
+                            other => Err(other).pipe(Some),
                         })
-                        .try_collect()?;
-                    match op {
-                        Operator::Add => Problem::Add(nums),
-                        Operator::Mul => Problem::Mul(nums),
-                    }
-                    .pipe(Ok)
+                        .map_ok(u64::from)
+                        .try_fold(None, |opt, res| match (opt, res) {
+                            (None, Ok(n)) => Some(n).pipe(Ok),
+                            (_, Err(err)) => Err(err),
+                            (Some(total), Ok(n)) => Some(total * 10 + n).pipe(Ok),
+                        })
+                        .map_err(|c| eyre!("{c} is not a valid digit"))
+                })
+                .chunk_by(|res| matches!(res, Ok(Some(_))))
+                .into_iter()
+                .filter(|(key, _)| *key)
+                .map(|(_, group)| {
+                    group
+                        .filter_map_ok(std::convert::identity)
+                        .try_collect::<_, Vec<_>, _>()
+                })
+                .zip(ops.split_whitespace())
+                .map(|(res, op)| match op {
+                    "+" => res.map(Problem::Add),
+                    "*" => res.map(Problem::Mul),
+                    other => bail!("{other:?} is not a valid operator"),
                 })
                 .try_collect()?,
         }
