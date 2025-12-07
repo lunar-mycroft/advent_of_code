afc1ade2747f6715f17cb53185cf911a9ae91da6
lunar-mycroft <lunar-mycroft@users.noreply.github.com>
2025-12-07 02:23:40 -0500

    Solve part 1 and part 2 in one pass

--- 2025/day-07/src/lib.rs
+++ 2025/day-07/src/lib.rs
@@ -29,6 +29,41 @@ impl std::str::FromStr for Puzzle {
     }
 }
 
+#[must_use]
+#[allow(clippy::needless_pass_by_value)]
+pub fn process(puzzle: Puzzle) -> (usize, usize) {
+    let mut counts: Grid<usize> = Grid::from_value(0, puzzle.grid.size());
+    counts[IVec2::new(puzzle.start, 1)] = 1;
+
+    let (mut paths_in_row, mut splits) = (0, 0);
+    for y in 1..puzzle.grid.size().y - 1 {
+        paths_in_row = 0;
+        for x in 0..puzzle.grid.size().x {
+            let pos = IVec2::new(x, y);
+            let paths = counts[pos];
+            if paths == 0 {
+                continue;
+            }
+
+            let new_pos = pos + IVec2::Y;
+            match puzzle.grid[new_pos] {
+                b'.' => {
+                    counts[new_pos] += paths;
+                    paths_in_row += paths;
+                }
+                b'^' => {
+                    counts[new_pos + IVec2::X] += paths;
+                    counts[new_pos - IVec2::X] += paths;
+                    paths_in_row += paths * 2;
+                    splits += 1;
+                }
+                _ => unreachable!(),
+            }
+        }
+    }
+    (splits, paths_in_row)
+}
+
 pub fn init_tracing() -> color_eyre::Result<()> {
     use tracing_subscriber::{layer::SubscriberExt, EnvFilter};
 
@@ -47,4 +82,26 @@ pub fn init_tracing() -> color_eyre::Result<()> {
         .with(tracing_error::ErrorLayer::default());
     tracing::subscriber::set_global_default(subscriber)?;
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use color_eyre::eyre::Result;
+    use rstest::rstest;
+
+    use super::*;
+
+    #[rstest]
+    #[case::example("example.txt", 21, 40)]
+    #[case::part1("part1.txt", 1672, 231_229_866_702_355)]
+    fn finds_solution(
+        #[case] input_path: &str,
+        #[case] part_1: usize,
+        #[case] part_2: usize,
+    ) -> Result<()> {
+        let input: Puzzle = common::read_input!(input_path).parse()?;
+        let output = process(input);
+        assert_eq!(output, (part_1, part_2));
+        Ok(())
+    }
 }
